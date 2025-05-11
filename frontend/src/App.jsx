fca3db417288e9393f0ada5eb279e58d157218bd
Gaurav Singh <gauravsingh1892000@gmail.com>
2025-05-11 10:41:09 +0530

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react"
+import Navbar from "./Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./Body"
+import Footer from "./Footer"
+import Login from "./Login"
+import Profile from "./Profile"
+
 
 
 function App() {
-  
-
   return (
-    <>
-     
-      <h1 className=" bg-amber-200 ">Hello World</h1>
-      </>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />} >
+        <Route path="/login" element={<Login/>} />
+        <Route path="/Profile" element={<Profile/>} />
+        </Route> 
+      </Routes>
+      </BrowserRouter>
   )
 }
-
 export default App
